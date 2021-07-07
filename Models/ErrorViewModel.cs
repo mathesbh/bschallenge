a85dbfc83d0a8d6b2023c8af05948322ddbf7209
Matheus Salomão <mathesbh@gmail.com>
2021-07-07 10:36:29 -0300

    Initial commit com configuração mysql e migration identy

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BSImoveis.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
