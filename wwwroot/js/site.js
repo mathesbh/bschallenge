b3be19517e6baaf36784a31af8e3e2a979128cf8
Matheus Salomão <mathesbh@gmail.com>
2021-07-08 14:19:05 -0300

    -Adiciona chamada API ViaCep -Preenchimento campo endereço por busca cep

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,33 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const inputCep = document.querySelector('#input-cep')
+inputCep.addEventListener('input', (e) => {
+  const cep = e.target.value;
+  handleApiCep(cep)
+});
 
-// Write your JavaScript code.
+function handleApiCep(cep){
+  const checkCep = /^[0-9]{8}$/;
+  if(checkCep.test(cep)){
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then(res => res.json())
+    .then(data => handleAddress(data))
+    .catch(err => console.log(err))
+  } else{
+    clearForm()
+  }
+}
+
+function handleAddress(data){
+  const street = data.logradouro;
+  const district = data.bairro;
+  const city = data.localidade;
+  const state = data.uf;
+  const address = `Rua: ${street}, Bairro: ${district}, ${city} - ${state}`;
+  const cep = data.cep;
+  document.querySelector('#address').value = address;
+  document.querySelector('#cep').value = cep.replace("-", "");
+}
+
+function clearForm(){
+  document.querySelector('#address').value = "";
+  document.querySelector('#cep').value = "";
+}
